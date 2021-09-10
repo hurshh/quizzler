0aa7d3ffc658afa124cc7a7ce58fae8040b0ff84
hurshh <quasarhrsh@gmail.com>
2021-09-10 18:26:09 +0530

    quizzler app is done

--- lib/main.dart
+++ lib/main.dart
@@ -20,72 +20,116 @@ class Quizzler extends StatelessWidget {
 }
 
 class QuizPage extends StatefulWidget {
+
   @override
   _QuizPageState createState() => _QuizPageState();
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = new List();
+  List<String> questions = ["Question 1","question 2","Question 3","Question 4","Question 5","end"];
+  List<bool> answers = [true,false,false,true,true];
+  int qIndex = 0;
+  int aIndex = 0;
+  Icon correct = Icon(Icons.check,color: Colors.green,);
+  Icon wrong = Icon(Icons.close,color: Colors.red,);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Expanded(
-          flex: 5,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                'This is where the question text will go.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Center(
+                child: Text(
+                  questions[qIndex],
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: FlatButton(
+                textColor: Colors.white,
+                color: Colors.green,
+                child: Text(
+                  'True',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
                 ),
+                onPressed: () {
+                  setState(() {
+                    if(scoreKeeper.length == 15){
+                      scoreKeeper.clear();
+                    }
+                    if(aIndex<answers.length && answers[aIndex]==true) {
+                      scoreKeeper.add(correct);
+                      qIndex++;
+                      aIndex++;
+                    }
+                    else if(aIndex<answers.length && answers[aIndex]==false){
+                      scoreKeeper.add(wrong);
+                      qIndex++;
+                      aIndex++;
+                    }
+                  });
+                  //The user picked true.
+                },
               ),
-              onPressed: () {
-                //The user picked true.
-              },
             ),
           ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: FlatButton(
+                color: Colors.red,
+                child: Text(
+                  'False',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
                 ),
+                onPressed: () {
+                  //The user picked false.
+                  setState(() {
+                    if(scoreKeeper.length == 15){
+                      scoreKeeper.clear();
+                    }
+                    if(aIndex<answers.length && answers[aIndex]==false) {
+                      scoreKeeper.add(correct);
+                      qIndex++;
+                      aIndex++;
+                    }
+                    else if(aIndex<answers.length && answers[aIndex]==true){
+                      scoreKeeper.add(wrong);
+                      qIndex++;
+                      aIndex++;
+                    }
+                  });
+                },
               ),
-              onPressed: () {
-                //The user picked false.
-              },
             ),
           ),
-        ),
-        //TODO: Add a Row here as your score keeper
-      ],
+          //TODO: Add a Row here as your score keeper
+
+          Row(
+            children: scoreKeeper,
+          )
+        ],
+      ),
     );
   }
 }
